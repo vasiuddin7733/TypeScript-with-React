946e62c89a1bb6d0cedd1ed77938a38517d7c3ef
maheedkhan7147 <v.mohammad@elsweyer-hoffmann.de>
2022-12-02 15:35:15 +0100

    add tailwind css file, and postcss files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='text-pink-500 text-6xl'>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
