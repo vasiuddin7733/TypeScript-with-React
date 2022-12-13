7dc4729fdfb5345e5c13c81edb307326f38eb093
maheedkhan7147 <v.mohammad@elsweyer-hoffmann.de>
2022-12-13 16:54:53 +0100

    add go back to home buttion and add padding to footer

--- frontend/src/layout/footer.jsx
+++ frontend/src/layout/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <h1 className="text-orange-500 mt-10 w-full text-3xl text-center">
+    <h1 className="text-orange-500 mt-10 w-full text-3xl text-center pb-20">
       Footer Component
     </h1>
   )
