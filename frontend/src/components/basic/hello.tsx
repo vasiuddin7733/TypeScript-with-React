7217658fd36a918eb344eaeb0da6baf54e7cdba9
vasiuddin7733 <vasiuddin7733>
2023-01-01 10:05:12 +0100

    update hello function

--- frontend/src/components/basic/hello.tsx
+++ frontend/src/components/basic/hello.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const hello = () => {
   return (
-    <div>hello, welcome to the typescript world 123</div>
+    <div>hello, welcome to the typescript world</div>
   )
 }
 
