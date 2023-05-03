f13a6bb8b372e50c9dcb4884b2552244dd482236
vasiuddin7733 <vasiuddin7733>
2023-05-03 10:05:12 +0200

    upodate the ides method

--- frontend/src/components/basic/idea.tsx
+++ frontend/src/components/basic/idea.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const idea = () => {
   return (
    <div>
-     <div>idea</div>
-    <div>idea1</div>
+     <div>idea1</div>
     <div>idea2</div>
     <div>idea3</div>
+    <div>idea4</div>
    </div>
   )
 }
